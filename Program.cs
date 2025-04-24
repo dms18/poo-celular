742d90cbeea90c60eac1071b8646a2575734990b
Diego <diegomarquessoare@gmail.com>
2025-04-24 16:55:36 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Iphone iphone = new Iphone("123456789", "iPhone 12", "123456789012345", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+iphone.ReceberLigacao();
+
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "987654321098765", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+nokia.ReceberLigacao();
